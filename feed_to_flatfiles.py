16fcdf11cfef93807a250df06e674a1869c6880d
Mike Jensen <jjensen.mike@gmail.com>
2012-05-07 16:50:47 -0400

    New conversion for feed to flatfiles. While the code needs to be cleaned up a bunch, this now runs in a fraction of the time

--- feed_to_flatfiles.py
+++ feed_to_flatfiles.py
@@ -1,24 +1,12 @@
-from sys import argv
 from lxml import etree
-from os.path import exists
-import schema
 import urllib
+import schema
 import os
 from csv import DictWriter
 
-#TODO: Use etree iterparse
-#TODO: Fix versions (pull schema for correct version of the feed
-#TODO: Clean up "extras" code
-
-fschema = exists('schema.xsd') and open('schema.xsd') \
-    or urllib.urlopen("http://election-info-standard.googlecode.com/files/vip_spec_v2.3.xsd")
-
-schema = schema.schema(fschema)
-
-simple_elements = schema.get_element_list("complexType", "simpleAddressType")
-detail_elements = schema.get_element_list("complexType", "detailAddressType")
-
-ELEMENT_LIST = schema.get_element_list("element","vip_object")
+fname = "large_test.xml"
+SCHEMA_URL = "http://election-info-standard.googlecode.com/files/vip_spec_v"
+VALID_VERSIONS = ["2.0","2.1","2.2","2.3","3.0"]
 
 def get_fields(element_name):
 	subschema = schema.get_sub_schema(element_name)
@@ -43,65 +31,78 @@ def get_fields(element_name):
 
 	return e_list
 
-fname = len(argv) >= 1 and argv[1] or 'test_feed.xml'
 
-xmlparser = etree.XMLParser()
-data = etree.parse(open(fname), xmlparser)
-root = data.getroot()
-elements = root.getchildren()
-cur_element = ""
-elem_fields = {}
+with open(fname) as xml_doc:
+	
+	context = etree.iterparse(xml_doc, events=("start", "end"))
+	context = iter(context)
+	
+	event, root = context.next()
+	
+	version = root.attrib["schemaVersion"] 
+	if version in VALID_VERSIONS:
+		if version == "2.2":
+			fschema = urllib.urlopen(SCHEMA_URL + version + "a.xsd")
+		else:
+			fschema = urllib.urlopen(SCHEMA_URL + version + ".xsd")		
 
-for element in elements:
+	schema = schema.Schema(fschema)
 
-	if element.tag in ELEMENT_LIST:
+	simple_elements = schema.get_element_list("complexType", "simpleAddressType")
+	detail_elements = schema.get_element_list("complexType", "detailAddressType")
 
-		if element.tag != cur_element:
+	ELEMENT_LIST = schema.get_element_list("element","vip_object")
+
+	elem_fields = {}
+	cur_element = ""
+
+	for event, element in context:
+		if event == "end" and element.tag in ELEMENT_LIST and len(element.getchildren()) > 0:
+			if element.tag != cur_element:
 			
-			cur_element = element.tag
+				cur_element = element.tag
 			
-			if os.path.exists(cur_element + ".txt"):
-				dw = DictWriter(open("flat_files/" + cur_element + ".txt", "a"), fieldnames=elem_fields[cur_element])
-			else:
-				elem_fields[cur_element] = get_fields(cur_element)
-				dw = DictWriter(open("flat_files/" + cur_element + ".txt", "w"), fieldnames=elem_fields[cur_element])
-				dw.writeheader()
-				print elem_fields[cur_element]
+				if os.path.exists("flat_files/" + cur_element + ".txt"):
+					dw = DictWriter(open("flat_files/" + cur_element + ".txt", "a"), fieldnames=elem_fields[cur_element])
+				else:
+					elem_fields[cur_element] = get_fields(cur_element)
+					dw = DictWriter(open("flat_files/" + cur_element + ".txt", "w"), fieldnames=elem_fields[cur_element])
+					dw.writeheader()
+					print elem_fields[cur_element]
 				 
-		element_dict = dict.fromkeys(elem_fields[cur_element], '')
-		element_dict["id"] = element.get("id")
+			element_dict = dict.fromkeys(elem_fields[cur_element], '')
+			element_dict["id"] = element.get("id")
 			
-		sub_elements = element.getchildren()
-		extras = []
+			sub_elements = element.getchildren()
+			extras = []
 		
-		for elem in sub_elements:
+			for elem in sub_elements:
 
-			if elem.tag.endswith("address"):
+				if elem.tag.endswith("address"):
 				
-				add_elems = elem.getchildren()
+					add_elems = elem.getchildren()
 				
-				for add_elem in add_elems:
-					element_dict[elem.tag + "_" + add_elem.tag] = add_elem.text
-			elif len(element_dict[elem.tag]) > 0:
-				extras.append({elem.tag:{"val":elem.text, "attributes":elem.attrib}}) 
-			else:
-				element_dict[elem.tag] = elem.text
+					for add_elem in add_elems:
+						element_dict[elem.tag + "_" + add_elem.tag] = add_elem.text
+				elif len(element_dict[elem.tag]) > 0:
+					extras.append({elem.tag:{"val":elem.text, "attributes":elem.attrib}}) 
+				else:
+					element_dict[elem.tag] = elem.text
 		
-		dw.writerow(element_dict)
-
-		for row in extras:
-			e_name = row.keys()[0]
-			element_dict[e_name] = row[e_name]["val"]
-			if len(row[e_name]["attributes"]) > 0:
-				for attr in row[e_name]["attributes"]:
-					element_dict[e_name + "_" + attr] = row[e_name]["attributes"][attr]
 			dw.writerow(element_dict)
-			element_dict[e_name] = ""
-			if len(row[e_name]["attributes"]) > 0:
-				for attr in row[e_name]["attributes"]:
-					element_dict[e_name + "_" + attr] = "" 
 
-	element.clear()
-	while elem.getprevious() is not None:
-		del elem.getparent()[0]
+			for row in extras:
+				e_name = row.keys()[0]
+				element_dict[e_name] = row[e_name]["val"]
+				if len(row[e_name]["attributes"]) > 0:
+					for attr in row[e_name]["attributes"]:
+						element_dict[e_name + "_" + attr] = row[e_name]["attributes"][attr]
+				dw.writerow(element_dict)
+				element_dict[e_name] = ""
+				if len(row[e_name]["attributes"]) > 0:
+					for attr in row[e_name]["attributes"]:
+						element_dict[e_name + "_" + attr] = ""
 
+			element.clear()
+			while element.getprevious() is not None:
+				del element.getparent()[0]
