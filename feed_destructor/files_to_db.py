d203fdb0a303c00a07035d577341e8f5163cf7a2
Mike Jensen <jjensen.mike@gmail.com>
2012-05-16 21:17:54 -0400

    Rewrote the script to insert files into the database, unfortunately duplicate and using insert-ignore for everything are causing this to run very slow, need to modify this to hold everything in memory and then write the table data much cleaner

--- feed_destructor/files_to_db.py
+++ feed_destructor/files_to_db.py
@@ -1,149 +1,139 @@
-from lxml import etree
+from schema import Schema
+from urllib import urlopen
 import MySQLdb as mdb
-import schema
-import urllib
+import sqlite3
 import os
 import csv
 
+SCHEMA_URL = "https://github.com/votinginfoproject/vip-specification/raw/master/vip_spec_v3.0.xsd"
 
-#TODO: accomodate for sort_order as part of the relational tables
-#TODO: change to github url
-fschema = urllib.urlopen("http://election-info-standard.googlecode.com/files/vip_spec_v3.0.xsd")
-#fschema = open("schema.xsd")
+FEED_DIR = "../flat_files/"
 
-schema = schema.schema(fschema)
+schema = Schema(urlopen(SCHEMA_URL))
 
-simpleAddressTypes = schema.get_elements_of_attribute("type", "simpleAddressType")
-detailAddressTypes = schema.get_elements_of_attribute("type", "detailAddressType")
+def get_fields(schema, elem_list):
+	
+	field_list = {}
+	
+	for elem_name in elem_list:
 
-ELEMENT_LIST = schema.get_element_list("element","vip_object")
-SIMPLECONTENTS = {)
-for elem in schema.schema["element"][0]["elements"]:
-	for e in elem["elements"]:
-		if "simpleContent" in e:
-			if e["name"] in SIMPLECONTENTS:
-				SIMPLECONTENTS[e["name"]]["parents"].append(elem['name'])
-			else:
-				SIMPLECONTENTS[e["name"]] = {"parents":[elem['name']]}
+		subschema = schema.get_sub_schema(elem_name)
+		e_list = {}
 
-UNBOUNDEDS = {}
-for elem in schema.schema["element"][0]["elements"]:
-	for e in elem["elements"]:
-		if "maxOccurs" in e and "simpleContent" not in e and e["maxOccurs"] == "unbounded":
-			if e["name"] in UNBOUNDEDS:
-				UNBOUNDEDS[e["name"]]["parents"].append(elem["name"])
-			else:
-				UNBOUNDEDS[e["name"]] = {"parents":[elem["name"]]}
+		if "elements" in subschema:
+			for e in subschema["elements"]:
+				e_name = e["name"]
+				if e["type"] == "simpleAddressType":
+					for s_e in simple_elements:
+						e_list[e_name + "_" + s_e] = "simpleAddressType_" + e_name
+				elif e["type"] == "detailAddressType":
+					for d_e in detail_elements:
+						e_list[e_name + "_" + d_e] = "detailAddressType_" + e_name
+				elif "maxOccurs" in e and e["maxOccurs"] == "unbounded":
+					e_list[e_name] = elem_name + "_" + e_name[:e_name.find("_id")]
+				else:
+					e_list[e_name] = elem_name
+				if "simpleContent" in e:
+					e_list[e_name] = elem_name + "_" + e_name[:e_name.find("_id")]
+					for sc_attr in e["simpleContent"]["attributes"]:
+						e_list[e_name + "_" + sc_attr["name"]] = elem_name + "_" + e_name[:e_name.find("_id")]
+		if "attributes" in subschema:
+			for a in subschema["attributes"]:
+				e_list[a["name"]] = elem_name			
+		field_list[elem_name] = {}
+		field_list[elem_name] = e_list
 
+	return field_list
+
+def insert_values(table_name, keys_list, values_list):
+	insert_statement = "INSERT IGNORE INTO " + table_name + " ("
+	insert_statement += ",".join(keys_list) + ") "
+	insert_statement += " VALUES ('" + "','".join(values_list) + "')"
+#	print insert_statement
+	cursor.execute(insert_statement)
+	connection.commit()
+	return cursor.lastrowid	
+	
+def insert_address(row_data, a_name, address_info):
+	keys_list = []
+	values_list = []
+	for a in address_info["elements"]:
+		values_list.append(row_data[a].replace("'", "").replace("/","-"))
+		keys_list.append(a[len(a_name)+1:])
+	return str(insert_values(address_info["type"], keys_list, values_list))
+
+def insert_other(row_data, elem_name, table_name, o_elements):
+	keys_list = ["vip_id", "election_id", elem_name+"_id"]
+	values_list = [str(vip_id), str(election_id), row_data["id"]]
+	for o in o_elements["elements"]:
+		values_list.append(row_data[o])
+		if o.startswith(o_elements["elements"][0]) and len(o) > len(o_elements["elements"][0]):
+			o = o[len(o_elements["elements"][0])+1:]
+		keys_list.append(o)
+	insert_values(table_name, keys_list, values_list)
+
+def insert_base_elements(row_data, table_name, base_elements):
+	keys_list = ["vip_id", "election_id", "feed_id"]
+	keys_list.extend(base_elements)
+	values_list = [str(vip_id), str(election_id), row_data["id"]]
+	for b in base_elements:
+		values_list.append(row_data[b].replace("'", "").replace("/","-"))
+	insert_values(table_name, keys_list, values_list)
+
+simple_elements = schema.get_element_list("complexType", "simpleAddressType")
+detail_elements = schema.get_element_list("complexType", "detailAddressType")
+element_list = schema.get_element_list("element","vip_object")
+elem_fields = get_fields(schema, element_list)
 vip_id = 32
-election_id = 2110
+election_id = 1000
+#connection = sqlite3.connect("localhost")
+connection = mdb.connect("localhost", "vip", "username", "password")
+cursor = connection.cursor()
 
-NORMALIZED_LIST = {"locality":10000000000, "precinct":20000000000,
-			"precinct_split":30000000000, "election_administration":40000000000,
-			"election_official":50000000000, "polling_location":60000000000,
-			"electoral_district":70000000000, "street_segment":80000000000,
-			"candidate":90000000000, "ballot":110000000000,
-			"contest":120000000000, "early_vote_site":130000000000,
-			"referendum":140000000000, "custom_ballot":150000000000,
-			"ballot_response":160000000000, "contest_result":170000000000,
-			"ballot_list_result":180000000000}
+for f in os.listdir(FEED_DIR):
+	ename = f.split(".")[0].lower()
 
-def insert_string(element_name, element_dict):
-
-	insert_str = "INSERT INTO " + element_name + " ("	
-	insert_str += ",".join(address_dict.keys()) + ") "
-	insert_str += "VALUES (\"" + "\",\"".join(address_dict.vals()) + "\")"
-	return insert_str
-
-def normalized_id(element_name, received_id):
-	if element_name == "source":
-		return str(1)
-	elif element_name == "election":
-		return str(election_id)
-	elif element_name == "state":
-		return str(received_id)
-	else:
-		return str(NORMALIZED_LIST[element_name] + int(received_id))
-
-def generate_id(element_name, row):
-	return "random " #TODO:Generate IDs from files without
-
-DIR = ""
-dir_list = os.listdir(DIR)
-for fname in dir_list:
-	ename = fname.split(".")[0].lower() 
-	if ename in ELEMENT_LIST:
+	if ename != "precinct_split": 
+		continue
 	
-		base_elements = []
+	if ename in element_list:
+
+		print "inserting: " + ename
+	
+		data = csv.DictReader(open(FEED_DIR + f))
+		header = data.fieldnames
+		ff_list = elem_fields[ename]
 		addresses = {}
-		relationals = []
-		has_id = False
+		others = {}
+		base_elements = []
 	
-		data = csv.DictReader(open(fname))
-		headers = data.fieldnames
-	
-		for header in headers:
-			if header == "vip_id":
-				continue
-			elif header == "election_id":
-				continue
-			elif header == "id":
-				has_id = True
-			elif header in ELEMENT_LIST["elements"]: #actuall need to change this to a schema call
-				base_elements.append(header)
-			elif header.endswith("_ids"):
-				relationals.append(header)
-			else:
-				for simpletype in simpleAddressTypes:
-					if header.startswith(simpletype):
-						if simpletype not in addresses:
-							addresses[simpletype] = {}
-							addresses[simpletype]["type"] = "simpleAddressType"
-							addresses[simpletype]["elements"] = []
-						addresses[simpletype]["elements"].append(header[len(simpletype)+1:])
-				for detailtype in detailAddressTypes:
-					if header.startswith(detailtype):
-						if detailtype not in addresses:
-							addresses[detailtype] = {}
-							addresses[detailtype]["type"] = "detailAddressType"
-							addresses[detailtype]["elements"] = []
-		
+		for h in header:
+			if ff_list[h].startswith("simpleAddressType"):
+				a_name = ff_list[h][len("simpleAddressType")+1:]
+				if a_name not in addresses:
+					addresses[a_name] = {"type":"simpleAddressType", "elements":[]}
+					base_elements.append(a_name+"_id")
+				addresses[a_name]["elements"].append(h)
+			elif ff_list[h].startswith("detailAddressType"):
+				a_name = ff_list[h][len("detailAddressType")+1:]
+				if a_name not in addresses:
+					addresses[a_name] = {"type":"detailAddressType", "elements":[]}
+					base_elements.append(a_name+"_id")
+				addresses[a_name]["elements"].append(h)
+			elif ff_list[h] != ename:
+				o_name = ff_list[h]
+				if o_name not in others:
+					others[o_name] = {"elements":[]}
+				others[o_name]["elements"].append(h)
+			elif h != "id":
+				base_elements.append(h)
+			
+
 		for row in data:
-			
-			insert_dict = {}
-			insert_dict["vip_id"] = vip_id
-			insert_dict["election_id"] = election_id
-			if has_id:
-				insert_dict["received_id"] = row["id"]
-				insert_dict["normalized_id"] = normalized_id(ename, row["id"])
-			else:
-				insert_dict["received_id"] = "0"
-				insert_dict["normalized_id"] = generate_id(ename, row)
-			
-			for address in addresses:
-				address_dict = {}
-				for element in address["elements"]:
-					address_dict[element] = row[address+"_"+element]
-				cursor.execute(insert_string(address["type"], address_dict))
-				insert_dict[address+"_id"] = cursor.lastrowid
-				connection.commit()
 
-			for relational in relationals:
-				relat_dict = {}
-				relat_dict["vip_id"] = vip_id
-				relat_dict["election_id"] = election_id
-				relat_dict[ename+"_id"] = insert_dict["normalized_id"]				
-
-				relat_ids = row[relational].split(",")
-				for i in range(len(relat_ids)):
-					relat_dict[relational[:-1]] = normalized_id(relational[:-4],relat_ids[i])
-					cursor.execute(insert_string(ename+"_"+relational[:-4],relat_dict))
-					connection.commit()
-
-			for element in base_elements:
-				insert_dict[element] = row[element]
-
-			cursor.execute(insert_string(ename, insert_dict))
-			connection.commit()
-					
+			for a_name in addresses:
+				row[a_name+"_id"] = insert_address(row, a_name, addresses[a_name])
+			for o_name in others:
+				insert_other(row, ename, o_name, others[o_name])
+			insert_base_elements(row, ename, base_elements)
 
