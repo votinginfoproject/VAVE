530b967667e105045dbc4d280238966c1081ad2e
Mike Jensen <jjensen.mike@gmail.com>
2012-06-10 17:53:34 -0400

    Fixes to flat file format checks

--- feed_destructor/feed_destructor.py
+++ feed_destructor/feed_destructor.py
@@ -82,7 +82,25 @@ def process_config(directory, config_file, schema_props):
 #check flat file format, if element format, check then convert to element format
 #if db format, check and then leave alone
 def process_flatfiles(directory, schema_props):
-	format_type = fc.flat_file_format(directory)
+
+	file_list = {} 
+	for f in os.listdir(directory):
+		fname, extension = f.lower().split(".")
+		if extension == "txt" or extension == "csv":
+			file_list[fname] = f
+
+	if any(fname not in schema_props.key_list("element") for fname in file_list.keys()):
+		if all(fname in schema_props.key_list("db") for fname in file_list.keys()):
+			invalid_files = fc.invalid_files(directory, file_list, schema_props.full_header_data("db"))
+		else:
+			print "sections error!!!"
+	else:
+		invalid_files = fc.invalid_files(directory, file_list, schema_props.full_header_data("element"))
+		#convert files that are not on the invalid files list into db files
+
+	for invalid in invalid_files:
+		os.remove(directory + invalid)
+	return invalid_files
 
 def write_and_archive(valid_files, vip_id):
 	
