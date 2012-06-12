32666005c3176cdb2eb41a013168546d506e8f91
Mike Jensen <jjensen.mike@gmail.com>
2012-06-11 21:00:11 -0400

    Conversion data from element format to db format

--- feed_destructor/schemaprops.py
+++ feed_destructor/schemaprops.py
@@ -35,12 +35,14 @@ class SchemaProps:
 
 		self.element_header = {}
 		self.db_header = {}
+		self.conversion_data = {}
 		
 		for elem_name in self.element_list:
 
 			subschema = self.schema.get_sub_schema(elem_name)
 			elem_list = []
 			db_list = []
+			conversion_list = {elem_name:{}}
 
 			if "elements" in subschema:
 				for e in subschema["elements"]:
@@ -49,30 +51,36 @@ class SchemaProps:
 						temp_list = self.address_fields(e_name, e["type"])
 						elem_list.extend(temp_list)
 						db_list.extend(temp_list)
+						for t in temp_list:
+							conversion_list[elem_name][t] = t
 					elif "simpleContent" in e:
 						elem_list.append(e_name)
 						sc_name = elem_name + "_" + e_name[:e_name.find("_id")]
+						conversion_list[sc_name] = {"id":elem_name + "_id", e_name:e_name}
 						temp_list = [elem_name + "_id", e_name]				 
 						for sc_attr in e["simpleContent"]["attributes"]:
 							elem_list.append(e_name + "_" + sc_attr["name"])
-							temp_list.append(e_name + "_" + sc_attr["name"])
+							temp_list.append(sc_attr["name"])
+							conversion_list[sc_name][e_name + "_" + sc_attr["name"]] = sc_attr["name"]
 						self.db_header[sc_name] = temp_list
 					elif "maxOccurs" in e and e["maxOccurs"] == "unbounded":
 						elem_list.append(e_name)
 						unbounded_name = elem_name + "_" + e_name[:e_name.find("_id")]
 						self.db_header[unbounded_name] = [elem_name + "_id", e_name]
+						conversion_list[unbounded_name] = {"id":elem_name + "_id", e_name:e_name}
 					else:
 						elem_list.append(e_name)
 						db_list.append(e_name)
+						conversion_list[elem_name][e_name] = e_name
 			if "attributes" in subschema:
 				for a in subschema["attributes"]:
 					elem_list.append(a["name"])
 					db_list.append(a["name"])
-			
+					conversion_list[elem_name][a["name"]] = a["name"]
 			self.element_header[elem_name] = elem_list
 			self.db_header[elem_name] = db_list
+			self.conversion_data[elem_name] = conversion_list
 	
-
 	def header(self, file_format, element):
 		if file_format == "db":
 			return self.db_header[element]
@@ -91,6 +99,12 @@ class SchemaProps:
 		elif file_format == "element":
 			return self.element_header.keys()
 
+	def get_conversion_data(self):
+		return self.conversion_data
+
+	def conversion_by_element(self, element):
+		return self.conversion_data[element]
+
 if __name__ == "__main__":
 	sp = SchemaProps()
 	print sp.header("db", "source")
@@ -98,3 +112,4 @@ if __name__ == "__main__":
 	print sp.key_list("element")
 	print sp.header("db", "precinct")
 	print sp.header("element", "precinct")
+	print sp.get_conversion_data()
