40294284068e99f57378383b99f2e71d6e7c0b29
Mike Jensen <jjensen.mike@gmail.com>
2012-07-12 16:35:57 -0400

    EasySQL error I forgot to push up

--- feed_destructor/easysql.py
+++ feed_destructor/easysql.py
@@ -6,7 +6,7 @@ class EasySQL:
 
 	def __init__(self, h='localhost', db='vip', u='username', pw='password'):
 		self.conn = psycopg2.connect(host=h,database=db,user=u,password=pw)
-		self.cursor = self.conn.cursor(cursor_factor=extras.RealDictCursor)
+		self.cursor = self.conn.cursor(cursor_factory=extras.RealDictCursor)
 
 	def basic_select(self, table, vals=None, conditions=None):
 		query = "SELECT "
@@ -60,6 +60,9 @@ class EasySQL:
 			query += " WHERE " + ' AND '.join(["{0} {1} '{2}'".format(k,conditions[k]['condition'],conditions[k]['compare_to']) for k in conditions])
 		self.cursor.execute(query)
 
+	#Right now this will use truncate if no conditions are supplied, since
+	#it will do more work cleaning up the table. If this gets too slow,
+	#this should be changed to just use "delete from" for the table
 	def delete(self, table, conditions=None):
 		if conditions:
 			conditions = self.clean_conditions(conditions)
