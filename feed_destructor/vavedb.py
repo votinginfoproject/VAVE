341658b9336946144ff0bc5b66e7e73128f1ea2f
Mike Jensen <jjensen.mike@gmail.com>
2012-07-12 13:49:47 -0400

    Updates to the db calls, making them more standardized

--- feed_destructor/vavedb.py
+++ feed_destructor/vavedb.py
@@ -1,3 +1,5 @@
+from easysql import EasySQL
+
 DEFAULT_ELECT_ID = 1000
 
 class VaveDB(EasySQL):
@@ -8,17 +10,18 @@ class VaveDB(EasySQL):
 		self.feed_id = v_id
 	
 	def get_election_id(self, feed_details):
-		result = self.select('elections','election_id',feed_details,1)
+		table_list = ['elections']
+		result = self.select(table_list,['election_id'],feed_details,1)
 		if not result:
-			last_id = self.select('elections','GREATEST(election_id)',None,1)
+			last_id = self.select(table_list,'GREATEST(election_id)',None,1)
 			if not last_id:
 				new_id = DEFAULT_ELECT_ID
 			else:
 				new_id = int(last_id["greatest"]) + 1
 			feed_details["election_id"] = new_id
-			self.insert('elections',feed_details)
+			self.insert(table_list[0],feed_details)
 		else:
-			feed_details["election_id"] = result["election_id"]
-		return feed_details
+			return result["election_id"]
+		return feed_details["election_id"]
 
 	
