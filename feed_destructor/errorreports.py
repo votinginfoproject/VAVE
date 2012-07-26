f09df0efcad5a45b94ea8fda8eca7c9511462bad
Mike Jensen <jjensen.mike@gmail.com>
2012-07-26 17:27:45 -0400

    A bunch of small changes to clear up reporting code (ie what details are where)

--- feed_destructor/errorreports.py
+++ feed_destructor/errorreports.py
@@ -13,61 +13,61 @@ def report_setup(vip_id=None):
 		dt.create_directory(REPORT_DIRECTORY+vip_id + "/archives")
 		dt.clear_or_create(REPORT_DIRECTORY+vip_id + "/current")
 
-def report_summary(feed_details, valid_files, invalid_files, invalid_sections):
+def report_summary(vip_id, election_id, file_details, election_details):
 	dt.create_directory(REPORT_DIRECTORY)
-	fname = "report_summary_" + feed_details["file_time_stamp"] + ".txt"
+	fname = "report_summary_" + file_details["file_timestamp"] + ".txt"
 
-	if "vip_id" not in feed_details:
+	if not vip_id:
 		directory = REPORT_DIRECTORY + "unknown"
 		dt.create_directory(directory)
 		with open(fname, "w") as w:
-			summary_header(feed_details, w)
-			file_summary(valid_files, invalid_files, invalid_sections, w)
+			summary_header(file_details, w)
+			file_summary(file_details, w)
 			w.write("Missing source information, could not process feed")
 	else:
-		directory = REPORT_DIRECTORY + str(feed_details["vip_id"]) + "/"
+		directory = REPORT_DIRECTORY + str(vip_id) + "/"
 		dt.create_directory(directory)
 		dt.create_directory(directory + "archives/")
 		dt.clear_or_create(directory + "current/")
 		with open(directory + "current/" + fname, "w") as w:
-			if "election_id" not in feed_details:
-				summary_header(feed_details, w)
-				file_summary(valid_files, invalid_files, invalid_sections, w)
+			summary_header(file_details, w)
+			source_summary(vip_id, file_details, w)
+			if not election_id:
+				file_summary(file_details, w)
 				w.write("Missing election information, could not process feed")
 			else:
-				summary_header(feed_details, w)
-				election_summary(feed_details, w)
-				file_summary(valid_files, invalid_files, invalid_sections, w)
+				election_summary(election_details, w)
+				file_summary(file_details, w)
 		copyfile(directory + "current/" + fname, directory + "archives/" + fname)
 
-def summary_header(feed_details, writer):
-	writer.write("File Processed: " + feed_details["file"] + "\n")
-	writer.write("Time Processed: " + feed_details["process_time"] + "\n\n")
+def summary_header(file_details, writer):
+	writer.write("File Processed: " + file_details["file"] + "\n")
+	writer.write("Time Processed: " + file_details["process_time"] + "\n\n")
 
-def file_summary(valid_files, invalid_files, invalid_sections, writer):
+def file_summary(file_details, writer):
 	writer.write("----------------------\nFile Report\n----------------------\n\n")
-	if len(invalid_sections) > 0:
-		writer.write("Invalid Sections: " + str(invalid_sections) + "\n")
-	if len(invalid_files) > 0:
-		writer.write("Invalid Files: " + str(invalid_files) + "\n")
-	if len(valid_files) > 0:
-		writer.write("Valid Files: " + str(valid_files) + "\n")
+	if "invalid_sections" in file_details:
+		writer.write("Invalid Sections: " + str(file_details["invalid_sections"]) + "\n")
+	if "invalid_files" in file_details:
+		writer.write("Invalid Files: " + str(file_details["invalid_files"]) + "\n")
+	if "valid_files" in file_details:
+		writer.write("Valid Files: " + str(file_details["valid_files"]) + "\n")
 	writer.write("\n")
 
-def source_summary(feed_details, writer):
+def source_summary(vip_id, feed_details, writer):
 	writer.write("----------------------\nSource Data\n----------------------\n\n")
-	writer.write("Name: " + feed_details["name"] + "\n")
-	writer.write("Vip ID: " + str(feed_details["vip_id"]) + "\n")
-	writer.write("Datetime: " + str(feed_details["datetime"]) + "\n\n")
+#	writer.write("Name: " + feed_details["name"] + "\n")
+	writer.write("Vip ID: " + str(vip_id) + "\n")
+#	writer.write("Datetime: " + str(feed_details["datetime"]) + "\n\n")
 
-def election_summary(feed_details, writer):
+def election_summary(election_details, writer):
 	writer.write("----------------------\nElection Data\n----------------------\n\n")
-	writer.write("Election ID: " + str(feed_details["election_id"]) + "\n")
-	writer.write("Election Date: " + str(feed_details["date"]) + "\n")
-	writer.write("Election Type: " + str(feed_details["election_type"]) + "\n\n")
+	writer.write("Election ID: " + str(election_details["election_id"]) + "\n")
+	writer.write("Election Date: " + str(election_details["date"]) + "\n")
+	writer.write("Election Type: " + str(election_details["election_type"]) + "\n\n")
 
-def feed_issues(vip_id, file_time_stamp, problem_data, issue_type):
-	fname = "feed_" + issue_type + "s_" + file_time_stamp + ".txt"
+def feed_issues(vip_id, file_timestamp, problem_data, issue_type):
+	fname = "feed_" + issue_type + "s_" + file_timestamp + ".txt"
 	cur_dir = REPORT_DIRECTORY + str(vip_id) + "/current/"
 	arc_dir = REPORT_DIRECTORY + str(vip_id) + "/archives/"
 	with open(cur_dir + fname, "a") as writer:
